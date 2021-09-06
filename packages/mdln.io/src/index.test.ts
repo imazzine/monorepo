1e3c4615ea5b3e972804799760e2c78ee8463dca
Artem Litvinov <artem.lit@lap-ua-artemlyt.local>
2021-09-06 10:50:23 +0300

    index.tets.ts removed

--- packages/mdln.io/src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import * as regular_import from "./index";
-
-describe("mdln.io exports", () => {
-  test("the regular export object is defined", () => {
-    expect(regular_import).toEqual({});
-  });
-});
