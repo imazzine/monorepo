c8b25cb11fcac4d0f4f657ed9512c67ade9b682c
Artem Litvinov <artem.lit@lap-ua-artemlyt.local>
2021-09-15 11:53:03 +0300

    init

---
+++ packages/mdln.io/src/enums/Errors.ts
@@ -0,0 +1,13 @@
+/**
+ * Errors codes enums.
+ */
+enum Errors {
+  CLI_ERR_UNKNOWN_OPTION = 1,
+  CLI_ERR_REQ_PATH_MISSED = 2,
+  CLI_ERR_FILE_NOT_FOUND = 3,
+  CLI_ERR_INVALID_HOST_VALUE = 4,
+  CLI_ERR_INVALID_PORT_VALUE = 5,
+  CLI_ERR_REQ_HOST_MISSED = 6,
+  CLI_ERR_REQ_PORT_MISSED = 7,
+}
+export default Errors;
---
+++ packages/mdln.io/src/types/public/Hub.ts
@@ -0,0 +1,25 @@
+import { Node } from "mdln";
+import CLI from "../internal/CLI";
+import NI from "../internal/NI";
+
+class Hub extends Node {
+  get cli(): CLI {
+    return this.children[0] as CLI;
+  }
+
+  get ni(): NI {
+    return this.children[1] as NI;
+  }
+
+  constructor() {
+    super();
+    this.logger.info(JSON.stringify({ instantiated: "Hub" }));
+
+    // ..
+    this.insert(new CLI(process.argv.slice(2)));
+    // ..
+
+    this.logger.info(JSON.stringify({ constructed: "Hub" }));
+  }
+}
+export default Hub;
