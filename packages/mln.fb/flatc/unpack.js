69ca29a44590cce192ece288ac69098cd171d6fd
imazzine <imazzine.development.kit@gmail.com>
2021-11-28 23:00:24 +0200

    flatc refactoring

--- packages/mln.fb/flatc/unpack.js
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-const AdmZip = require("adm-zip");
-(new AdmZip(path.resolve(__dirname, `./${process.platform}.zip`)))
-  .extractAllTo(path.resolve(__dirname), true);
