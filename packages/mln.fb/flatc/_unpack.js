5bb747383ff8f679670ddc14648aea2959b539e6
imazzine <imazzine.development.kit@gmail.com>
2021-11-28 23:34:00 +0200

    fb wip

--- packages/mln.fb/flatc/_unpack.js
+++ packages/mln.fb/flatc/_unpack.js
@@ -1,5 +1,13 @@
+const fs = require('fs');
 const path = require('path');
 (new require("adm-zip")(
   path.resolve(__dirname,
   `./${process.platform}.zip`)
 )).extractAllTo(path.resolve(__dirname), true);
+fs.chmodSync(
+  path.resolve(
+    __dirname,
+    `flatc${process.platform === 'win32' ? '.exe' : ''}`
+  ),
+  0o766
+);
